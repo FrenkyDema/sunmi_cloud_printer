41adf8a2d3916a2c9521c622e840586ea68bbc6f
FrenkyDema <frenkydema@gmail.com>
2022-08-03 10:27:22 +0200

    ConnectionTest

--- android/src/main/java/com/example/sunmi_cloud_printer/SunmiCloudPrinterMethod.java
+++ android/src/main/java/com/example/sunmi_cloud_printer/SunmiCloudPrinterMethod.java
@@ -2,32 +2,40 @@ package com.example.sunmi_cloud_printer;
 
 
 import android.content.Context;
-import android.os.Bundle;
-import android.view.View;
+
 import com.sunmi.externalprinterlibrary.api.ConnectCallback;
 import com.sunmi.externalprinterlibrary.api.PrinterException;
 import com.sunmi.externalprinterlibrary.api.SunmiPrinter;
 import com.sunmi.externalprinterlibrary.api.SunmiPrinterApi;
 
+import java.util.List;
+
 public class SunmiCloudPrinterMethod {
 
-    private Context _context;
+    private final Context _context;
 
     public SunmiCloudPrinterMethod(Context _context) {
         this._context = _context;
     }
 
-    void setCloudPrinter(View view) throws PrinterException {
+    void setCloudPrinter() throws PrinterException {
         SunmiPrinterApi.getInstance().setPrinter(SunmiPrinter.SunmiCloudPrinter);
 
     }
 
-    void setNetPrinter(View view) throws PrinterException {
+    void setBTPrinter() throws PrinterException {
+        List<String> macList = SunmiPrinterApi.getInstance().findBleDevice(_context);
+        if (macList.size() > 0) {
+            SunmiPrinterApi.getInstance().setPrinter(SunmiPrinter.SunmiBlueToothPrinter, macList.get(0));
+        }
+    }
+
+    void setNetPrinter() throws PrinterException {
         String ip = "192.168.2.93";
         SunmiPrinterApi.getInstance().setPrinter(SunmiPrinter.SunmiNetPrinter, ip);
     }
 
-    void connect(View view) throws PrinterException {
+    void connect() throws PrinterException {
         if (!SunmiPrinterApi.getInstance().isConnected()) {
             SunmiPrinterApi.getInstance().connectPrinter(_context, new ConnectCallback() {
                 @Override
@@ -53,5 +61,22 @@ public class SunmiCloudPrinterMethod {
             });
         }
     }
+
+    void printerInit() throws PrinterException {
+        SunmiPrinterApi.getInstance().printerInit();
+    }
+
+    void printerText(String text) throws PrinterException {
+        SunmiPrinterApi.getInstance().printText(text);
+    }
+
+    void lineWrap(int nLine) throws PrinterException {
+        SunmiPrinterApi.getInstance().lineWrap(nLine);
+    }
+
+    void printQrCode(String data) throws PrinterException {
+        SunmiPrinterApi.getInstance().printQrCode("123456789", 4, 0);
+    }
+
 
 }
--- android/src/main/java/com/example/sunmi_cloud_printer/SunmiCloudPrinterPlugin.java
+++ android/src/main/java/com/example/sunmi_cloud_printer/SunmiCloudPrinterPlugin.java
@@ -2,37 +2,57 @@ package com.example.sunmi_cloud_printer;
 
 import androidx.annotation.NonNull;
 
+import com.sunmi.externalprinterlibrary.api.PrinterException;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
-/** SunmiCloudPrinterPlugin */
+/**
+ * SunmiCloudPrinterPlugin
+ */
 public class SunmiCloudPrinterPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private static SunmiCloudPrinterMethod sunmiCloudPrinterMethod;
+    private MethodChannel channel;
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "sunmi_cloud_printer");
-    channel.setMethodCallHandler(this);
-  }
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "sunmi_cloud_printer");
+        sunmiCloudPrinterMethod =
+                new SunmiCloudPrinterMethod(flutterPluginBinding.getApplicationContext());
+        channel.setMethodCallHandler(this);
     }
-  }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-  }
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        switch (call.method) {
+            case "TEST": {
+                try {
+                    sunmiCloudPrinterMethod.setNetPrinter();
+                    sunmiCloudPrinterMethod.connect();
+                    sunmiCloudPrinterMethod.printerInit();
+                    sunmiCloudPrinterMethod.printerText("Test di prova!");
+                    sunmiCloudPrinterMethod.printQrCode("123456789");
+                    sunmiCloudPrinterMethod.lineWrap(3);
+                } catch (PrinterException exception) {
+                    exception.printStackTrace();
+                }
+
+            }
+
+            default: {
+            }
+        }
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
 }
